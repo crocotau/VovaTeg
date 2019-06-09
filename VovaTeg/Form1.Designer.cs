6b8cd353eebf54f4de827450ec5817231a46f1ad
crocotau <goshkout@gmail.com>
2019-06-09 15:38:56 +0300

    Мелочь

---
+++ VovaTeg/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace VovaTeg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.fb = new System.Windows.Forms.FolderBrowserDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(32, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Выбрать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.progressBar1.Location = new System.Drawing.Point(32, 120);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(177, 22);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(29, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 23);
+            this.label1.TabIndex = 2;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(32, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Заменить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 158);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(254, 197);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(254, 197);
+            this.Name = "Form1";
+            this.Text = "VovaTeg";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog fb;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ VovaTeg/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ookii.Dialogs.WinForms;
+using File = System.IO.File;
+
+namespace VovaTeg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string Translit(string str)
+        {
+            string[] lat_up = { "A", "B", "V", "G", "D", "E", "Yo", "Zh", "Z", "I", "Y", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "F", "Kh", "Ts", "Ch", "Sh", "Shch", "\"", "Y", "'", "E", "Yu", "Ya" };
+            string[] lat_low = { "a", "b", "v", "g", "d", "e", "yo", "zh", "z", "i", "y", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "kh", "ts", "ch", "sh", "shch", "\"", "y", "'", "e", "yu", "ya" };
+            string[] rus_up = { "А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я" };
+            string[] rus_low = { "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я" };
+            for (int i = 0; i <= 32; i++)
+            {
+                str = str.Replace(rus_up[i], lat_up[i]);
+                str = str.Replace(rus_low[i], lat_low[i]);
+            }
+            return str;
+        }
+
+        VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
+        
+        async void ChangeFiles()
+        {
+            label1.Text = "";
+            label1.Visible = false;
+            label1.BorderStyle = BorderStyle.None;
+            dialog.Description = "Выбери папку с музыкой";
+            dialog.ShowNewFolderButton = true;
+            dialog.UseDescriptionForTitle = true;
+
+
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var path = dialog.SelectedPath;
+
+                //var myFiles = Path.GetFileNameWithoutExtension(path).ToList();
+
+                var myFiles = Directory.EnumerateFiles(path, "*.*")
+                    .Select(p => Path.GetFileName(p))
+                    .Where(s => s.EndsWith(".mp3", StringComparison.OrdinalIgnoreCase)).ToArray();
+
+
+                foreach (var VARIABLE in myFiles)
+                {
+                   File.Move(path.ToString() + '\\'
+                                                  + VARIABLE, path.ToString() + '\\'
+                                                                              + Translit(VARIABLE.ToString()
+                                                                                  .ToUpper().Replace(".MP3", ".mp3")));
+                }
+                
+                    await Task.Delay(2000);
+                    button2.Enabled = true;
+                    button2.Focus();
+                    label1.Visible = true;
+                    label1.BorderStyle = BorderStyle.None;
+                    label1.Text = "Нажми \"Заменить\"";
+
+            }
+            else
+            {
+                label1.Visible = true;
+                label1.BorderStyle = BorderStyle.None;
+                label1.Text = "Нажми \"Выбрать\"";
+            }
+        }
+
+
+        async void ChangeTag()
+        {
+            progressBar1.Visible = true;
+            label1.Text = "";
+            label1.Visible = false;
+            label1.BorderStyle = BorderStyle.None;
+            progressBar1.Value = 0;
+
+            var path = dialog.SelectedPath;
+            var myFiles = Directory.EnumerateFiles(path, "*.*")
+                .Select(p => Path.GetFileName(p))
+                .Where(s => s.EndsWith(".mp3", StringComparison.OrdinalIgnoreCase)).ToArray();
+
+
+            foreach (var VARIABLE in myFiles)
+            {
+                File.SetAttributes(path + "\\" + VARIABLE, FileAttributes.Normal);
+                var tfile = TagLib.File.Create(path + "\\" + VARIABLE);
+                tfile.Tag.Clear();
+                tfile.Save();
+            }
+
+            int qt = myFiles.Length;
+            progressBar1.Maximum = qt;
+
+            while (progressBar1.Value < qt)
+            {
+                int percent = (int)(((double)progressBar1.Value / (double)progressBar1.Maximum) * 100);
+                progressBar1.Refresh();
+                progressBar1.CreateGraphics().DrawString(percent.ToString() + "%",
+                    new Font("Arial", (float)10, FontStyle.Regular),
+                    Brushes.Black,
+                    new PointF(progressBar1.Width / 2 - 10, progressBar1.Height / 2 - 7));
+
+                await Task.Run(() =>
+                {
+                    foreach (var VARIABLE in myFiles)
+                    {
+                        var tfile = TagLib.File.Create(path + "\\" + VARIABLE);
+                        tfile.Tag.Title = tfile.Name.Replace(path, "").Remove(0, 1)
+                            .Replace(".mp3", "");
+
+                        tfile.Save();
+                    }
+                });
+
+                progressBar1.Value++;
+
+                if (progressBar1.Value == qt)
+                {
+                    //await Task.Delay(500);
+                    label1.BorderStyle = BorderStyle.None;
+                    label1.Visible = true;
+                    label1.Text = "Готово!";
+                    button1.Focus();
+                    button2.Enabled = false;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChangeFiles();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ChangeTag();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar1.Visible = false;
+            button2.Enabled = false;
+            label1.Visible = true;
+            label1.BorderStyle = BorderStyle.None;
+            label1.Text = "Нажми \"Выбрать\"";
+        }
+    }
+}
